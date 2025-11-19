7690edcf1058f6ef10197dcc1d6e97e8e79efe3a
reyhan-dot <reyhanalakmal@gmail.com>
2025-11-19 17:09:56 +0700

    Add files via upload

---
+++ praktikum 2 tugas 2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>;
+using namespace std;
+int main()
+{
+	double nilai;
+	cout << "=== Program Penghitungan Nilai ===" << endl;
+	cout << "Masukkan nilai angka:";
+	cin >> nilai;
+
+	cout << "\nhasil:" << endl;
+	if (nilai > 76) {
+		cout << "Nilai mutu:A" << endl;
+		cout << "kerja bagus" << endl;
+	}
+	else if (nilai >70 && nilai <76) {
+		cout << "Nilai mutu:B" << endl;
+		cout << "Cukup untuk lulus" << endl;
+	}
+	else if (nilai > 65 && nilai <70) {
+		cout << "Nilai mutu:c" << endl;
+		cout << "Bisa dibenahi" << endl;
+	}
+	else if (nilai >50 && nilai <65) {
+		cout << "Nilai mutu:D" << endl;
+		cout << "Butuh nilai tambahan" << endl;
+	}
+	else if (nilai <= 50) {
+		cout << "Nilai mutu:E" << endl;
+		cout << " Perlu belajar  " << endl;
+	}
+}
+
+	
